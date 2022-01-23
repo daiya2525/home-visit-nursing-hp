f160c6c56ebc66f04fd34911635b15ed7e93f010
inomoto daiya <daiandaian2525@gmail.com>
2022-01-24 01:10:29 +0900

    footer編集

--- src/components/organisms/layout/Footer.tsx
+++ src/components/organisms/layout/Footer.tsx
@@ -1,21 +1,45 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { memo, VFC } from "react";
+import { Link } from "react-router-dom";
 
 import styles from "../../../styles/footer/footer.module.css";
+import companyLogo from "../../../static/images/nimil.png";
 
 export const Footer: VFC = memo(() => {
   return (
     <footer>
       <div className={styles.footerWrap}>
         <div className={styles.footerInfo}>
-          <h1>訪問看護ステーション</h1>
+          <div className={styles.Logo}>
+            <img src={companyLogo} alt="会社ロゴ写真です" />
+            <span></span>
+            <p>訪問看護ステーション</p>
+          </div>
           <p className={styles.address}>
             〒581-0882 大阪府八尾市恩智北町3丁目8番 2階
           </p>
-          <a href="tel:080-4018-0882">tel.080-4018-0882</a>
+          <div className={styles.infoInner}>
+            <a href="tel:080-4018-0882">tel.080-4018-0882</a>
+            <div>
+              <Link to="/contact">お問合せはこちらから</Link>
+            </div>
+          </div>
+          <div className={styles.page_transition}></div>
+          <p className={styles.copy_right}>
+            © HOME VISIT, All Rights Reserved.
+          </p>
         </div>
-        <div className={styles.map_canvas}></div>
-        <p className={styles.copy_right}>aiueo</p>
+        <div className={styles.map_canvas}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.840228909214!2d135.62834045822694!3d34.61224090261365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001271a709630d9%3A0xf416008f659ac84a!2z44CSNTgxLTA4ODIg5aSn6Ziq5bqc5YWr5bC-5biC5oGp5pm65YyX55S677yS5LiB55uu77yW77yT4oiS77yY!5e0!3m2!1sja!2sjp!4v1642952166783!5m2!1sja!2sjp"
+            width="532"
+            height="460"
+            aria-hidden="false"
+            loading="lazy"
+          ></iframe>
+          "
+        </div>
       </div>
     </footer>
   );
